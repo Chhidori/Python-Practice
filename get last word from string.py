fa507b39d80337dc125f5052608f24621d660b27
chhidori <silentpacifier09@gmail.com>
2022-11-16 17:37:24 +0530

    first Commit

---
+++ get last word from string.py
@@ -0,0 +1,4 @@
+str =input(("Enter the String:"))
+result = str.split()
+lastword = result.pop()
+print(f'The last Word of the string is: {lastword}')
---
+++ reversing string.py
@@ -0,0 +1,3 @@
+value = input(("Enter the String:"))
+rev_str = value[::-1]
+print(f'The Reversed String is: {rev_str}')
